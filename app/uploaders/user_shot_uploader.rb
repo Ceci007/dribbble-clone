c7c26ae399c4fe069d622304c3b5be1346f7e4bf
solbencas <cecibenitezca@gmail.com>
2020-10-25 15:32:45 -0300

    Modify carrierwave.rb and user_shot_uploader.rb

--- app/uploaders/user_shot_uploader.rb
+++ app/uploaders/user_shot_uploader.rb
@@ -4,11 +4,7 @@ class UserShotUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :aws
-  else
-    storage :file
-  end
+  storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
